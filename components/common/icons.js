30cc14d0f3dbe9c1944c91bfc048a3dbc27ed510
timknapp12 <timknapp12@gmail.com>
2019-10-29 01:42:31 -0600

    revised icons

--- components/common/icons.js
+++ components/common/icons.js
@@ -1,37 +1,29 @@
 import React from "react";
-import RhoTree from "../icons/RhoTree";
-import RhoTreeWhite from "../icons/RhoTreeWhite";
-import RhoTreeBlack from "../icons/RhoTreeBlack";
-import CartBlack from "../icons/CartBlack";
-import CartWhite from "../icons/CartWhite";
-import CartBlack2 from "../icons/CartBlack2";
+import RhoLogo from "../icons/RhoLogo";
+import CartIcon from "../icons/CartIcon";
+import LavenderToBlueLogo from "../icons/LavenderToBlueLogo";
+import LavenderToBlueCart from "../icons/LavenderToBlueCart";
 
-export const BlackLogoText = props => <RhoTree fill={props.fill} />;
-
-export const WhiteLogo = props => (
-  <RhoTreeWhite
-    style={{
-      width: props.size,
-      stroke: props.color,
-      fill: props.color
-    }}
+export const LavenderToBlueCartIcon = props => (
+  <LavenderToBlueCart
+    width={props.size}
+    fill={props.color}
+    stroke={props.color}
   />
 );
 
-export const BlackLogo = props => (
-  <RhoTreeBlack
-    style={{ width: props.size, stroke: props.color, fill: props.color }}
+export const CartLogo = props => (
+  <CartIcon width={props.size} fill={props.color} stroke={props.color} />
+);
+
+export const LavenderToBlueIcon = props => (
+  <LavenderToBlueLogo
+    width={props.size}
+    fill={props.color}
+    stroke={props.color}
   />
 );
 
-export const BlackCart = props => (
-  <CartBlack width={props.size} fill={props.color} stroke={props.color} />
-);
-
-export const WhiteCart = props => (
-  <CartWhite width={props.size} fill={props.color} stroke={props.color} />
-);
-
-export const BlackCart2 = props => (
-  <CartBlack2 width={props.size} fill={props.color} stroke={props.color} />
+export const Logo = props => (
+  <RhoLogo width={props.size} fill={props.color} stroke={props.color} />
 );
--- stories/3-Navbar.stories.js
+++ stories/3-Navbar.stories.js
@@ -3,13 +3,17 @@ import { storiesOf } from "@storybook/react";
 import Nav from "../components/Nav";
 import styled from "@emotion/styled";
 import {
-  WhiteLogo,
-  WhiteCart,
-  BlackLogo,
-  BlackCart,
-  BlackCart2
+  CartLogo,
+  LavenderToBlueCartIcon,
+  LavenderToBlueIcon,
+  Logo
 } from "../components/common/icons";
-import { lavenderToBlue, seafoam } from "../components/common/style-constants";
+import {
+  lavenderToBlue,
+  seafoam,
+  lavender,
+  blue
+} from "../components/common/style-constants";
 
 const Wrapper = styled.div`
   display: grid;
@@ -43,31 +47,56 @@ const GridItemWrapperTwo = styled.div`
 `;
 
 storiesOf("Navbar", () => module)
-  .add("default", () => (
+  .add("seafoam background and gradient icons", () => (
     <Nav background={seafoam}>
-      <p style={{ color: "white" }}>
-        Hello there this is a cool baavbar that i made and it is very fin to
-        make although my fingers hurt form typing cus Imm not bthat good at it,
-        but I try ans at leat that is aa stateint point so eventually hopefullu
-        I'll be good sosmeday, lthough my fingers hurt form typing cus Imm not
-        bthat good at it, but I try ans at leat that is aa stateint point so
-        eventually hopefullu I'll be good sosmeday
-      </p>
+      <Wrapper>
+        <GridItemWrapperTwo start={1} justify="flex-start">
+          <LavenderToBlueIcon size="4rem" />
+        </GridItemWrapperTwo>
+        <GridItemWrapperTwo start={2} end={5} justify="center">
+          <h2 style={{ fontSize: "2rem", margin: 0, color: lavender }}>
+            Restore Hope Oils
+          </h2>
+        </GridItemWrapperTwo>
+        <GridItemWrapperTwo start={5} justify="flex-end">
+          <LavenderToBlueCartIcon size="3rem" />
+        </GridItemWrapperTwo>
+      </Wrapper>
     </Nav>
   ))
-  .add("lavender to blue", () => (
+  .add("lavender to blue with white logos", () => (
     <div style={{ height: "100vh", border: "1px dotted black" }}>
       <Nav background={lavenderToBlue}>
         <Wrapper>
           <GridItemWrapper start={1} justify="flex-start">
-            <WhiteLogo size="4rem" />
+            <Logo size="4rem" color="white" />
           </GridItemWrapper>
           <GridItemWrapper start={2} end={5} justify="center">
             <h2 style={{ fontSize: "2rem", margin: 0 }}>Restore Hope Oils</h2>
           </GridItemWrapper>
           <GridItemWrapper start={5} justify="flex-end">
-            <WhiteCart size="3rem" />
+            <CartLogo size="3rem" color="white" />
           </GridItemWrapper>
+        </Wrapper>
+      </Nav>
+      <p>Here is some random stuff on the page</p>
+    </div>
+  ))
+  .add("lavender to blue with seafoam logos", () => (
+    <div style={{ height: "100vh", border: "1px dotted black" }}>
+      <Nav background={lavenderToBlue}>
+        <Wrapper>
+          <GridItemWrapperTwo start={1} justify="flex-start">
+            <Logo size="4rem" color={seafoam} />
+          </GridItemWrapperTwo>
+          <GridItemWrapperTwo start={2} end={5} justify="center">
+            <h2 style={{ fontSize: "2rem", margin: 0, color: seafoam }}>
+              Restore Hope Oils
+            </h2>
+          </GridItemWrapperTwo>
+          <GridItemWrapperTwo start={5} justify="flex-end">
+            <CartLogo size="3rem" color={seafoam} />
+          </GridItemWrapperTwo>
         </Wrapper>
       </Nav>
       <p>Here is some random stuff on the page</p>
@@ -78,7 +107,7 @@ storiesOf("Navbar", () => module)
       <Nav background="white">
         <Wrapper>
           <GridItemWrapperTwo start={1} justify="flex-start">
-            <BlackLogo size="4rem" />
+            <Logo size="4rem" />
           </GridItemWrapperTwo>
           <GridItemWrapperTwo start={2} end={5} justify="center">
             <h2 style={{ fontSize: "2rem", margin: 0, color: "black" }}>
@@ -86,7 +115,93 @@ storiesOf("Navbar", () => module)
             </h2>
           </GridItemWrapperTwo>
           <GridItemWrapperTwo start={5} justify="flex-end">
-            <BlackCart2 size="3rem" color={seafoam} />
+            <CartLogo size="3rem" />
+          </GridItemWrapperTwo>
+        </Wrapper>
+      </Nav>
+      <p>Here is some random stuff on the page</p>
+    </div>
+  ))
+  .add("White background with seafoam", () => (
+    <div style={{ height: "100vh", border: "1px dotted black" }}>
+      <Nav background="white">
+        <Wrapper>
+          <GridItemWrapperTwo start={1} justify="flex-start">
+            <Logo size="4rem" color={seafoam} />
+          </GridItemWrapperTwo>
+          <GridItemWrapperTwo start={2} end={5} justify="center">
+            <h2 style={{ fontSize: "2rem", margin: 0, color: seafoam }}>
+              Restore Hope Oils
+            </h2>
+          </GridItemWrapperTwo>
+          <GridItemWrapperTwo start={5} justify="flex-end">
+            <CartLogo size="3rem" color={seafoam} />
+          </GridItemWrapperTwo>
+        </Wrapper>
+      </Nav>
+      <p>Here is some random stuff on the page</p>
+    </div>
+  ))
+  .add("White background with lavender", () => (
+    <div style={{ height: "100vh", border: "1px dotted black" }}>
+      <Nav background="white">
+        <Wrapper>
+          <GridItemWrapperTwo start={1} justify="flex-start">
+            <Logo size="4rem" color={lavender} />
+          </GridItemWrapperTwo>
+          <GridItemWrapperTwo start={2} end={5} justify="center">
+            <h2 style={{ fontSize: "2rem", margin: 0, color: lavender }}>
+              Restore Hope Oils
+            </h2>
+          </GridItemWrapperTwo>
+          <GridItemWrapperTwo start={5} justify="flex-end">
+            <CartLogo size="3rem" color={lavender} />
+          </GridItemWrapperTwo>
+        </Wrapper>
+      </Nav>
+      <p>Here is some random stuff on the page</p>
+    </div>
+  ))
+  .add("White background with blue", () => (
+    <div style={{ height: "100vh", border: "1px dotted black" }}>
+      <Nav background="white">
+        <Wrapper>
+          <GridItemWrapperTwo start={1} justify="flex-start">
+            <Logo size="4rem" color={blue} />
+          </GridItemWrapperTwo>
+          <GridItemWrapperTwo start={2} end={5} justify="center">
+            <h2 style={{ fontSize: "2rem", margin: 0, color: blue }}>
+              Restore Hope Oils
+            </h2>
+          </GridItemWrapperTwo>
+          <GridItemWrapperTwo start={5} justify="flex-end">
+            <CartLogo size="3rem" color={blue} />
+          </GridItemWrapperTwo>
+        </Wrapper>
+      </Nav>
+      <p>Here is some random stuff on the page</p>
+    </div>
+  ))
+  .add("White background with gradient", () => (
+    <div style={{ height: "100vh", border: "1px dotted black" }}>
+      <Nav background="white">
+        <Wrapper>
+          <GridItemWrapperTwo start={1} justify="flex-start">
+            <LavenderToBlueIcon size="4rem" />
+          </GridItemWrapperTwo>
+          <GridItemWrapperTwo start={2} end={5} justify="center">
+            <h2
+              style={{
+                fontSize: "2rem",
+                margin: 0,
+                background: lavenderToBlue
+              }}
+            >
+              Restore Hope Oils
+            </h2>
+          </GridItemWrapperTwo>
+          <GridItemWrapperTwo start={5} justify="flex-end">
+            <LavenderToBlueCartIcon size="3rem" />
           </GridItemWrapperTwo>
         </Wrapper>
       </Nav>
