b0fb39cb3f35fa527f6660a68c2e14425368a1ea
timknapp12 <timknapp12@gmail.com>
2019-11-11 17:16:06 -0700

    Home page (#2)
    * added colored svgs
    
    * revised icons
    
    * added black to nav
    
    * added coral to stories

---
+++ components/icons/LavenderToBlueCart.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+const SvgLavenderToBlueCart = props => (
+  <svg viewBox="0 0 400 400" {...props}>
+    <linearGradient
+      id="LavenderToBlueCart_svg__a"
+      gradientUnits="userSpaceOnUse"
+      x1={180.222}
+      y1={350.592}
+      x2={141.138}
+      y2={311.508}
+    >
+      <stop offset={0} stopColor="#55a4ca" />
+      <stop offset={1} stopColor="#967bb6" />
+    </linearGradient>
+    <path
+      d="M160.68 303.4c-15.25 0-27.65 12.4-27.65 27.65s12.4 27.65 27.65 27.65c15.25 0 27.65-12.4 27.65-27.65s-12.4-27.65-27.65-27.65z"
+      fill="url(#LavenderToBlueCart_svg__a)"
+    />
+    <linearGradient
+      id="LavenderToBlueCart_svg__b"
+      gradientUnits="userSpaceOnUse"
+      x1={310.778}
+      y1={299.792}
+      x2={60.029}
+      y2={49.043}
+    >
+      <stop offset={0} stopColor="#55a4ca" />
+      <stop offset={1} stopColor="#967bb6" />
+    </linearGradient>
+    <path
+      d="M112.66 252.08c26.42.03 143.22.14 192.14.18h5.92l34.84-113.48-15.29-4.7-31.37 102.17c-56.2-.05-179.66-.17-190.8-.18-4.2-14.73-8.76-30.74-13.34-46.79L67.05 92.06l-49.12-.92-.3 16 37.28.7 24.47 85.83c10.18 35.72 20.32 71.29 26.46 92.82l1.66 5.81h210.79v-16H119.57c-2.02-7.07-4.35-15.27-6.91-24.22z"
+      fill="url(#LavenderToBlueCart_svg__b)"
+    />
+    <linearGradient
+      id="LavenderToBlueCart_svg__c"
+      gradientUnits="userSpaceOnUse"
+      x1={288.612}
+      y1={350.592}
+      x2={249.528}
+      y2={311.508}
+    >
+      <stop offset={0} stopColor="#55a4ca" />
+      <stop offset={1} stopColor="#967bb6" />
+    </linearGradient>
+    <path
+      d="M269.07 303.4c-15.25 0-27.65 12.4-27.65 27.65s12.4 27.65 27.65 27.65 27.65-12.4 27.65-27.65-12.4-27.65-27.65-27.65z"
+      fill="url(#LavenderToBlueCart_svg__c)"
+    />
+  </svg>
+);
+
+export default SvgLavenderToBlueCart;
--- stories/3-Navbar.stories.js
+++ stories/3-Navbar.stories.js
@@ -3,12 +3,17 @@ import { storiesOf } from "@storybook/react";
 import Nav from "../components/Nav";
 import styled from "@emotion/styled";
 import {
-  WhiteLogo,
-  WhiteCart,
-  BlackLogo,
-  BlackCart
+  CartLogo,
+  LavenderToBlueCartIcon,
+  LavenderToBlueIcon,
+  Logo
 } from "../components/common/icons";
-import { lavenderToBlue, seafoam } from "../components/common/style-constants";
+import {
+  lavenderToBlue,
+  seafoam,
+  lavender,
+  blue
+} from "../components/common/style-constants";
 
 const Wrapper = styled.div`
   display: grid;
@@ -42,31 +47,56 @@ const GridItemWrapperTwo = styled.div`
 `;
 
 storiesOf("Navbar", () => module)
-  .add("default", () => (
+  .add("seafoam background and gradient icons", () => (
     <Nav background={seafoam}>
-      <p style={{ color: "white" }}>
-        Hello there this is a cool baavbar that i made and it is very fin to
-        make although my fingers hurt form typing cus Imm not bthat good at it,
-        but I try ans at leat that is aa stateint point so eventually hopefullu
-        I'll be good sosmeday, lthough my fingers hurt form typing cus Imm not
-        bthat good at it, but I try ans at leat that is aa stateint point so
-        eventually hopefullu I'll be good sosmeday
-      </p>
+      <Wrapper>
+        <GridItemWrapperTwo start={1} justify="flex-start">
+          <LavenderToBlueIcon size="4rem" />
+        </GridItemWrapperTwo>
+        <GridItemWrapperTwo start={2} end={5} justify="center">
+          <h2 style={{ fontSize: "2rem", margin: 0, color: lavender }}>
+            Restore Hope Oils
+          </h2>
+        </GridItemWrapperTwo>
+        <GridItemWrapperTwo start={5} justify="flex-end">
+          <LavenderToBlueCartIcon size="3rem" />
+        </GridItemWrapperTwo>
+      </Wrapper>
     </Nav>
   ))
-  .add("lavender to blue", () => (
+  .add("lavender to blue with white logos", () => (
     <div style={{ height: "100vh", border: "1px dotted black" }}>
       <Nav background={lavenderToBlue}>
         <Wrapper>
           <GridItemWrapper start={1} justify="flex-start">
-            <WhiteLogo size="4rem" />
+            <Logo size="4rem" color="white" />
           </GridItemWrapper>
           <GridItemWrapper start={2} end={5} justify="center">
             <h2 style={{ fontSize: "2rem", margin: 0 }}>Restore Hope Oils</h2>
           </GridItemWrapper>
           <GridItemWrapper start={5} justify="flex-end">
-            <WhiteCart size="3rem" />
+            <CartLogo size="3rem" color="white" />
           </GridItemWrapper>
+        </Wrapper>
+      </Nav>
+      <p>Here is some random stuff on the page</p>
+    </div>
+  ))
+  .add("lavender to blue with seafoam logos", () => (
+    <div style={{ height: "100vh", border: "1px dotted black" }}>
+      <Nav background={lavenderToBlue}>
+        <Wrapper>
+          <GridItemWrapperTwo start={1} justify="flex-start">
+            <Logo size="4rem" color={seafoam} />
+          </GridItemWrapperTwo>
+          <GridItemWrapperTwo start={2} end={5} justify="center">
+            <h2 style={{ fontSize: "2rem", margin: 0, color: seafoam }}>
+              Restore Hope Oils
+            </h2>
+          </GridItemWrapperTwo>
+          <GridItemWrapperTwo start={5} justify="flex-end">
+            <CartLogo size="3rem" color={seafoam} />
+          </GridItemWrapperTwo>
         </Wrapper>
       </Nav>
       <p>Here is some random stuff on the page</p>
@@ -77,7 +107,7 @@ storiesOf("Navbar", () => module)
       <Nav background="white">
         <Wrapper>
           <GridItemWrapperTwo start={1} justify="flex-start">
-            <BlackLogo size="4rem" />
+            <Logo size="4rem" />
           </GridItemWrapperTwo>
           <GridItemWrapperTwo start={2} end={5} justify="center">
             <h2 style={{ fontSize: "2rem", margin: 0, color: "black" }}>
@@ -85,10 +115,236 @@ storiesOf("Navbar", () => module)
             </h2>
           </GridItemWrapperTwo>
           <GridItemWrapperTwo start={5} justify="flex-end">
-            <BlackCart size="3rem" />
+            <CartLogo size="3rem" />
           </GridItemWrapperTwo>
         </Wrapper>
       </Nav>
       <p>Here is some random stuff on the page</p>
+    </div>
+  ))
+  .add("White background with coral", () => (
+    <div style={{ height: "100vh", border: "1px dotted black" }}>
+      <Nav background="white">
+        <Wrapper>
+          <GridItemWrapperTwo start={1} justify="flex-start">
+            <Logo size="4rem" color="coral" />
+          </GridItemWrapperTwo>
+          <GridItemWrapperTwo start={2} end={5} justify="center">
+            <h2 style={{ fontSize: "2rem", margin: 0, color: "coral" }}>
+              Restore Hope Oils
+            </h2>
+          </GridItemWrapperTwo>
+          <GridItemWrapperTwo start={5} justify="flex-end">
+            <CartLogo size="3rem" color="coral" />
+          </GridItemWrapperTwo>
+        </Wrapper>
+      </Nav>
+      <p>Here is some random stuff on the page</p>
+    </div>
+  ))
+  .add("White background with lavender", () => (
+    <div style={{ height: "100vh", border: "1px dotted black" }}>
+      <Nav background="white">
+        <Wrapper>
+          <GridItemWrapperTwo start={1} justify="flex-start">
+            <Logo size="4rem" color={lavender} />
+          </GridItemWrapperTwo>
+          <GridItemWrapperTwo start={2} end={5} justify="center">
+            <h2 style={{ fontSize: "2rem", margin: 0, color: lavender }}>
+              Restore Hope Oils
+            </h2>
+          </GridItemWrapperTwo>
+          <GridItemWrapperTwo start={5} justify="flex-end">
+            <CartLogo size="3rem" color={lavender} />
+          </GridItemWrapperTwo>
+        </Wrapper>
+      </Nav>
+      <p>Here is some random stuff on the page</p>
+    </div>
+  ))
+  .add("White background with blue", () => (
+    <div style={{ height: "100vh", border: "1px dotted black" }}>
+      <Nav background="white">
+        <Wrapper>
+          <GridItemWrapperTwo start={1} justify="flex-start">
+            <Logo size="4rem" color={blue} />
+          </GridItemWrapperTwo>
+          <GridItemWrapperTwo start={2} end={5} justify="center">
+            <h2 style={{ fontSize: "2rem", margin: 0, color: blue }}>
+              Restore Hope Oils
+            </h2>
+          </GridItemWrapperTwo>
+          <GridItemWrapperTwo start={5} justify="flex-end">
+            <CartLogo size="3rem" color={blue} />
+          </GridItemWrapperTwo>
+        </Wrapper>
+      </Nav>
+      <p>Here is some random stuff on the page</p>
+    </div>
+  ))
+  .add("White background with gradient", () => (
+    <div style={{ height: "100vh", border: "1px dotted black" }}>
+      <Nav background="white">
+        <Wrapper>
+          <GridItemWrapperTwo start={1} justify="flex-start">
+            <LavenderToBlueIcon size="4rem" />
+          </GridItemWrapperTwo>
+          <GridItemWrapperTwo start={2} end={5} justify="center">
+            <h2
+              style={{
+                fontSize: "2rem",
+                margin: 0,
+                background: `linear-gradient(${lavender}, ${blue})`,
+                "-webkit-background-clip": "text",
+                "-webkit-text-fill-color": "transparent"
+              }}
+            >
+              Restore Hope Oils
+            </h2>
+          </GridItemWrapperTwo>
+          <GridItemWrapperTwo start={5} justify="flex-end">
+            <LavenderToBlueCartIcon size="3rem" />
+          </GridItemWrapperTwo>
+        </Wrapper>
+      </Nav>
+      <p>Here is some random stuff on the page</p>
+    </div>
+  ))
+  .add("black background", () => (
+    <div
+      style={{
+        height: "100vh",
+        border: "1px dotted black",
+        background: "#252526"
+      }}
+    >
+      <Nav background="black">
+        <Wrapper>
+          <GridItemWrapperTwo start={1} justify="flex-start">
+            <Logo size="4rem" color="white" />
+          </GridItemWrapperTwo>
+          <GridItemWrapperTwo start={2} end={5} justify="center">
+            <h2 style={{ fontSize: "2rem", margin: 0, color: "white" }}>
+              Restore Hope Oils
+            </h2>
+          </GridItemWrapperTwo>
+          <GridItemWrapperTwo start={5} justify="flex-end">
+            <CartLogo size="3rem" color="white" />
+          </GridItemWrapperTwo>
+        </Wrapper>
+      </Nav>
+      <p style={{ color: "white" }}>Here is some random stuff on the page</p>
+    </div>
+  ))
+  .add("black background with seafoam", () => (
+    <div
+      style={{
+        height: "100vh",
+        border: "1px dotted black",
+        background: "#252526"
+      }}
+    >
+      <Nav background="black">
+        <Wrapper>
+          <GridItemWrapperTwo start={1} justify="flex-start">
+            <Logo size="4rem" color={seafoam} />
+          </GridItemWrapperTwo>
+          <GridItemWrapperTwo start={2} end={5} justify="center">
+            <h2 style={{ fontSize: "2rem", margin: 0, color: seafoam }}>
+              Restore Hope Oils
+            </h2>
+          </GridItemWrapperTwo>
+          <GridItemWrapperTwo start={5} justify="flex-end">
+            <CartLogo size="3rem" color={seafoam} />
+          </GridItemWrapperTwo>
+        </Wrapper>
+      </Nav>
+      <p style={{ color: "white" }}>Here is some random stuff on the page</p>{" "}
+    </div>
+  ))
+  .add("black background with lavender", () => (
+    <div
+      style={{
+        height: "100vh",
+        border: "1px dotted black",
+        background: "#252526"
+      }}
+    >
+      <Nav background="black">
+        <Wrapper>
+          <GridItemWrapperTwo start={1} justify="flex-start">
+            <Logo size="4rem" color={lavender} />
+          </GridItemWrapperTwo>
+          <GridItemWrapperTwo start={2} end={5} justify="center">
+            <h2 style={{ fontSize: "2rem", margin: 0, color: lavender }}>
+              Restore Hope Oils
+            </h2>
+          </GridItemWrapperTwo>
+          <GridItemWrapperTwo start={5} justify="flex-end">
+            <CartLogo size="3rem" color={lavender} />
+          </GridItemWrapperTwo>
+        </Wrapper>
+      </Nav>
+      <p style={{ color: "white" }}>Here is some random stuff on the page</p>{" "}
+    </div>
+  ))
+  .add("black background with blue", () => (
+    <div
+      style={{
+        height: "100vh",
+        border: "1px dotted black",
+        background: "#252526"
+      }}
+    >
+      <Nav background="black">
+        <Wrapper>
+          <GridItemWrapperTwo start={1} justify="flex-start">
+            <Logo size="4rem" color={blue} />
+          </GridItemWrapperTwo>
+          <GridItemWrapperTwo start={2} end={5} justify="center">
+            <h2 style={{ fontSize: "2rem", margin: 0, color: blue }}>
+              Restore Hope Oils
+            </h2>
+          </GridItemWrapperTwo>
+          <GridItemWrapperTwo start={5} justify="flex-end">
+            <CartLogo size="3rem" color={blue} />
+          </GridItemWrapperTwo>
+        </Wrapper>
+      </Nav>
+      <p style={{ color: "white" }}>Here is some random stuff on the page</p>{" "}
+    </div>
+  ))
+  .add("black background with gradient", () => (
+    <div
+      style={{
+        height: "100vh",
+        border: "1px dotted black",
+        background: "#252526"
+      }}
+    >
+      <Nav background="black">
+        <Wrapper>
+          <GridItemWrapperTwo start={1} justify="flex-start">
+            <LavenderToBlueIcon size="4rem" />
+          </GridItemWrapperTwo>
+          <GridItemWrapperTwo start={2} end={5} justify="center">
+            <h2
+              style={{
+                fontSize: "2rem",
+                margin: 0,
+                background: `-webkit-linear-gradient(${lavender}, ${blue})`,
+                "-webkit-background-clip": "text",
+                "-webkit-text-fill-color": "transparent"
+              }}
+            >
+              Restore Hope Oils
+            </h2>
+          </GridItemWrapperTwo>
+          <GridItemWrapperTwo start={5} justify="flex-end">
+            <LavenderToBlueCartIcon size="3rem" />
+          </GridItemWrapperTwo>
+        </Wrapper>
+      </Nav>
+      <p style={{ color: "white" }}>Here is some random stuff on the page</p>{" "}
     </div>
   ));
