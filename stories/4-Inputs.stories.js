6e4aa768a756d9e5cd4571159f09ca0854b88050
timknapp12 <timknapp12@gmail.com>
2019-11-13 23:03:57 -0700

    added an input

---
+++ stories/4-Inputs.stories.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import Input from "../components/inputs";
+// import styled from "@emotion/styled";
+
+storiesOf("inputs", () => module).add("default", () => <Input />);
