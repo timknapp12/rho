8254aea3b671556927b0edc8507024f88a5e6028
timknapp12 <timknapp12@gmail.com>
2020-03-12 22:30:16 -0600

    Home page (#3)
    * added colored svgs
    
    * revised icons
    
    * added black to nav
    
    * added coral to stories
    
    * added an input

---
+++ components/inputs/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+const StyledInput = styled.input`
+  border-color: red;
+`;
+
+const Input = () => <StyledInput />;
+
+export default Input;
